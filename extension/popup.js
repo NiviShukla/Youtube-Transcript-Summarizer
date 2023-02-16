79a31e5d88c8d726714ab605778f540ba420f3a4
Nivesh Shukla <52473549+NiviShukla@users.noreply.github.com>
2023-02-16 22:15:38 +0530

    Add files via upload

---
+++ extension/popup.js
@@ -0,0 +1,19 @@
+const btn = document.getElementById("summarise");
+btn.addEventListener("click", function() {
+    btn.disabled = true;
+    btn.innerHTML = "Summarising...";
+    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+        var url = tabs[0].url;
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "http://127.0.0.1:5000/summary?url=" + url, true);
+        xhr.onload = function() {
+            var text = xhr.responseText;
+            const p = document.getElementById("output");
+            p.innerHTML = text;
+            p.style.opacity = 1;
+            btn.disabled = false;
+            btn.innerHTML = "Summarise";
+        }
+        xhr.send();
+    });
+});
